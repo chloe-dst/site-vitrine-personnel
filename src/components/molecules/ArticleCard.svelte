7bded645e49b9139033b3139f7c76653f9ec6e78
Doustalet Chloé <cdoustalet@gmail.com>
2024-10-28 17:30:44 +0100

    Add ArticleCard component and update Blog component

---
+++ src/components/molecules/ArticleCard.svelte
@@ -0,0 +1,65 @@
+<script>
+  import Icon from "../atoms/Icon.svelte";
+  import Link from "../atoms/Link.svelte";
+  
+  import ArrowBackgroundIcon from "../../assets/icons/ArrowBackgroundIcon.svelte";
+
+  export let articleUrl = "/blog/{value.slug}";
+</script>
+
+<div class="article-card">
+  <slot name="image" />
+  <Link linkUrl={articleUrl}> 
+    <slot name="linkName" />
+  </Link>
+  <div class="link-arrow">
+    <div class="blog-circle"> </div>
+    <Link linkUrl="{articleUrl}">
+      <Icon name="Arrow_background" width="80" height="80" iconColor>
+        <ArrowBackgroundIcon />
+      </Icon>
+    </Link>
+  </div>
+</div>
+
+<style>
+  .article-card {
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 380px;
+    background-color: var(--color-background-secondary);
+    border-radius: var(--large-radius);
+    position: relative;
+    padding: 0;
+  }
+
+  :global(.blog-container img){
+    border-radius: var(--large-radius) var(--large-radius) 0 0;
+  }
+
+  .blog-circle{
+    border-radius:50%;
+    width:114px;
+    height:114px;
+    background-color: var(--color-background-primary);
+    position: absolute;
+    right: -15px;
+    bottom: -15px;
+  }
+
+  :global(.blog-container .article-card > .link){
+    width: 68%;
+    height: 85px;
+    font-size: 20px;
+    font-family: var(--font-family-tercary);
+    font-weight: 600;
+    padding: 25px;
+    line-height: 26px;
+  }
+
+  :global(.link-arrow .link){
+    position: absolute;
+    right: 0;
+    bottom: 0;
+  }
+</style>
--- src/components/organisms/Blog.svelte
+++ src/components/organisms/Blog.svelte
@@ -1,10 +1,9 @@
 <script>
+	import ArticleCard from "../molecules/ArticleCard.svelte";
   import Image from "../atoms/Image.svelte";
 	import Link from "../atoms/Link.svelte";
   import Icon from "../atoms/Icon.svelte";
   import Pagination from "../atoms/Pagination.svelte";
-
-  import ArrowBackgroundIcon from "../../assets/icons/ArrowBackgroundIcon.svelte";
 
   let values = [];
   export let data;
@@ -14,21 +13,15 @@
   <ul class="blog-container block-center">
     {#each values as value}
       <li>
-        <Image
-          imageSrc={value.image}
-          imageAlt="Couverture article de blog sur {value.title}"
-          imageWidth=380
-          slot="image"
-        />
-        <Link linkUrl="/blog/{value.slug}"> {value.title} </Link>
-        <div class="link-arrow">
-          <div class="blog-circle"> </div>
-          <Link linkUrl="/blog/{value.slug}">
-            <Icon name="Arrow_background" width="80" height="80" iconColor>
-              <ArrowBackgroundIcon />
-            </Icon>
-          </Link>
-        </div>
+        <ArticleCard articleUrl="/blog/{value.slug}">
+          <Image
+            imageSrc={value.image} 
+            imageAlt="Couverture article de blog sur {value.title}"
+            imageWidth=380
+            slot="image"
+          />
+          <span slot="linkName"> {value.title} </span>
+        </ArticleCard>
       </li>
     {/each}
   </ul>
@@ -44,46 +37,6 @@
     column-gap: var(--spacing-5);
     row-gap: var(--spacing-5);
     margin: var(--spacing-4);
-  }
-
-  .blog-container li {
-    display: flex;
-    flex-wrap: wrap;
-    max-width: 380px;
-    background-color: var(--color-background-secondary);
-    border-radius: var(--large-radius);
-    position: relative;
-    padding: 0;
-  }
-
-  :global(.blog-container img){
-    border-radius: var(--large-radius) var(--large-radius) 0 0;
-  }
-
-  .blog-circle{
-    border-radius:50%;
-    width:114px;
-    height:114px;
-    background-color: var(--color-background-primary);
-    position: absolute;
-    right: -15px;
-    bottom: -15px;
-  }
-
-  :global(.blog-container li > .link){
-    width: 68%;
-    height: 85px;
-    font-size: 20px;
-    font-family: var(--font-family-tercary);
-    font-weight: 600;
-    padding: 25px;
-    line-height: 26px;
-  }
-
-  :global(.link-arrow .link){
-    position: absolute;
-    right: 0;
-    bottom: 0;
   }
 </style>
   
